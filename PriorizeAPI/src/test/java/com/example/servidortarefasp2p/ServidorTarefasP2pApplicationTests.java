12fd1d20f787b8006f61e3d5bde6acc6d38c04b6
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2026-06-22 05:41:52 -0300

    feat: setup inicial do servidor spring boot com websocket. closes #1

---
+++ PriorizeAPI/src/test/java/com/example/servidortarefasp2p/ServidorTarefasP2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidortarefasp2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorTarefasP2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
